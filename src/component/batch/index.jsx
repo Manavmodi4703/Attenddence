047823f9ef24ecd245a60ec037beaba22142f07b
Manavmodi4703 <Manavmodi4703@gmail.com>
2023-08-13 10:33:27 +0530

    check1

--- src/component/batch/index.jsx
+++ src/component/batch/index.jsx
@@ -1,22 +1,43 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router'
 
 const Batch = () => {
   const[year,setYear] = useState("")
+  const[section,setSection] = useState("")
+  const[subject, setSubject] = useState("")
+const navigate = useNavigate() 
 
-  year=setYear
+  const selectYear = (e) => {
+    const selectedYear = e.target.value;
+    setYear(selectedYear);
+
   return (
     <>
-    <form action=''>
-    <select required onChange={(e) => setYear(e.target.value)}>
+    <form action='' value={year} onChange={selectYear}>
+    <select required >
             <option selected disabled value="Year">Select the Year</option>
             <option value="First">First</option>
             <option value="Second">Second</option>
             <option value="Third">Third</option>
             <option value="Fourth">Fourth</option>
         </select>
+      <select required value={section} onChange={selectYear}>
+      <option selected disabled value="Section">Select the Section</option>
+            <option value="A">A</option>
+            <option value="B">B</option>
+            <option value="C">C</option>
+            <option value="D">D</option>
+      </select>
+      <select required value={subject} onChange={selectYear}>
+      <option selected disabled value="Subject">Select the Subject</option>
+            <option value="TOC">TOC</option>
+            <option value="DCCN">DCCN</option>
+            <option value="MI">MI</option>
+            <option value="DBMS">DBMS</option>
+      </select>
     </form>
     </>
   )
 }
-
-export default Batch
+}
+export default Batch;
--- src/component/register/index.jsx
+++ src/component/register/index.jsx
@@ -2,13 +2,24 @@ import React, { useState } from 'react'
 import { useNavigate } from 'react-router'
 
 const Register = () => {
-  const [role, setRole] = useState("")
+  const [role, setRole] = useState(null)
   const navigate = useNavigate()
+
+  const handleRoleChange = (e) => {
+    const selectedRole = e.target.value;
+    setRole(selectedRole)
+if(selectedRole == "Faculty"){
+  navigate("/faculty")
+ }
+ else{
+  navigate("/student")
+ }
+};
   return (
-    <select onChange = {(e) => setRole(e.target.value)}>
+    <select value={role} onChange={handleRoleChange}>
         <option disabled selected value="select your role">Select Your Role</option>
-        <option value="Faculty">Faculty</option>
-        <option value="Student">Student</option>
+        <option value="Faculty" >Faculty</option>
+        <option value="Student" >Student</option>
     </select>
   )
     }
